45ae0804c7d14fb55064b29310e44f2688ffd232
LEBLOND Ludovic <137878701+deltaTreize@users.noreply.github.com>
2023-11-10 17:02:47 +0000

    2h

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,39 @@
+const leftArrow = document.querySelector(".arrow_left");
+const rightArrow = document.querySelector(".arrow_right");
+const bannerImg = document.getElementById("banner-img");
+
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "./asset/images/slideshow/slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "./assets/images/slideshow/slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "./assets/images/slideshow/slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "./assets/images/slideshow/slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+
+// leftArrow.addEventListener("click", () => {
+
+// })
+rightArrow.addEventListener("click", rightClick);
+
+
+function rightClick() {
+  if (i <= slides.length, i++) {
+    bannerImg.src = slides[i].image;
+		console.log(i);
+  }
+	// if (i>slides.length){
+	// 	i=0;
+	// }
+}
