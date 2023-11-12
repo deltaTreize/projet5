e2588286debc2880cc849f524427b419876fbf70
LEBLOND Ludovic <137878701+deltaTreize@users.noreply.github.com>
2023-11-12 17:02:12 +0000

    fini +2h total 4h

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,13 @@
 const leftArrow = document.querySelector(".arrow_left");
 const rightArrow = document.querySelector(".arrow_right");
 const bannerImg = document.getElementById("banner-img");
-
+const bannerP = document.querySelector(".banner p");
+const dots = document.querySelectorAll(".dot");
+const dotsArray = Array.from(dots);
+let i = 0; 
 const slides = [
   {
-    image: "./asset/images/slideshow/slide1.jpg",
+    image: "./assets/images/slideshow/slide1.jpg",
     tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
   },
   {
@@ -22,18 +25,12 @@ const slides = [
   },
 ];
 
-// leftArrow.addEventListener("click", () => {
-
-// })
-rightArrow.addEventListener("click", rightClick);
-
-
-function rightClick() {
-  if (i <= slides.length, i++) {
-    bannerImg.src = slides[i].image;
-		console.log(i);
-  }
-	// if (i>slides.length){
-	// 	i=0;
-	// }
+function handleChange(count) {
+  dotsArray[i].classList.remove("dot_selected");
+  i = i + count;
+  if (i >= slides.length) { i = 0};
+  if (i < 0) { i= slides.length -1};
+  dotsArray[i].classList.add("dot_selected");
+  bannerImg.src = slides[i].image;
+  bannerP.innerHTML = slides[i].tagLine;
 }
